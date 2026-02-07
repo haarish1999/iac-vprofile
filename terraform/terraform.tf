c333d02e8c50431cf265aba27848147549a50c24
haarish1999 <haarish1999@gmail.com>
2026-02-07 11:16:44 +0530

    test5 self

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "harishownuse19"
+    bucket = "subbu2409"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.14"
